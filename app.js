23b24b966b018b1137a2e89f919136613e99a24c
Arun Kumar Borru <arunkumar.b@holosuit.com>
2022-07-26 13:05:07 +0530

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +17,23 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent , posts : posts})
- // console.log(posts);
+  res.render("home", { homeStartingContent: homeStartingContent, posts: posts })
+
 })
 
+app.get("/posts/:topic", (req, res) => {
+  const reqTitle = _.lowerCase(req.params.topic);
+  var title ="";
+  var content = "";
+  posts.forEach(function (post) {
+    const availTitle = _.lowerCase(post.title);
+    if (reqTitle === availTitle) {
+      title = post.title;
+      content = post.content;
+    }
+  })
+  res.render("post", { title : title , content , content })
+});
 app.get("/about", (req, res) => {
   res.render("about", { aboutContent: aboutContent })
 })
@@ -35,12 +49,12 @@ app.get("/compose", (req, res) => {
 })
 
 
-app.post("/compose", (req, res) => {  
-  const post = {
-    title : req.body.title,
-    content : req.body.postText
+app.post("/compose", (req, res) => {
+  var post = {
+    title: req.body.title,
+    content: req.body.postText
   };
-  posts.push(posts);
+  posts.push(post);
   res.redirect("/")
 })
 
